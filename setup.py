14e2d6bd77171795f14e67e342d4686aa9e22b93
medinadiegoeverardo <diegoedina.a@gmail.com>
2019-10-29 00:36:14 -0700

    upload to pypi

---
+++ setup.py
@@ -0,0 +1,29 @@
+"""
+lambdata - a collection of data science helper functions for lambda school
+"""
+
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas",
+    "requests"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+    setuptools.setup(
+    name="ran_words_md",
+    version = "0.1.1",
+    author = "medinadiego",
+    description = "col of ds helper functions",
+    long_description = LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://lambdaschool.com/courses/data-science",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires = REQUIRED,
+    classifiers=["Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ])
